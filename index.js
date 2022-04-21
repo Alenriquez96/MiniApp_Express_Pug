bffc3241da6df890d44d70c086681fa40d4d7b08
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-21 21:00:55 +0200

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 const formRouter = require("./routes/productsRoutes")
 
 app.set("view engine", "pug");
